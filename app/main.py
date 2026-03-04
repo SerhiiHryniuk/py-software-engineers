7ee33e968801912b63f7b5153744b796c42711bf
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-03-04 15:41:09 +0200

    Fix: added annotations, changed to extend, added init in fullstack developer

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 class SoftwareEngineer:
     def __init__(self, name: str) -> None:
         self.name = name
-        self.skills = []
+        self.skills: list[str] = []
 
     def learn_skill(self, skill: str) -> None:
-        self.skills += [skill]
+        self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        skills = ["JavaScript", "HTML", "CSS"]
-        for skill in skills:
-            super().learn_skill(skill)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -22,9 +20,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        skills = ["Python", "SQL", "Django"]
-        for skill in skills:
-            super().learn_skill(skill)
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -34,9 +30,7 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        skills = ["Java", "Android studio"]
-        for skill in skills:
-            super().learn_skill(skill)
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -44,7 +38,10 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-        super().create_powerful_api()
-        super().create_awesome_web_page()
+        self.create_powerful_api()
+        self.create_awesome_web_page()
